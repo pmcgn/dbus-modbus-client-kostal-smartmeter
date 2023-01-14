b7f5680957d31682d47c6425e550765b061bc222
Philipp Mahler <development@pmcgn.de>
2023-01-14 23:39:42 +0100

    Initial Commit

---
+++ Kostal_SmartEnergyMeter.py
@@ -0,0 +1,100 @@
+# VenusOS module for support of KOSTAL Smart Energy Meter
+# 
+# Community contribution by Philipp Mahler
+# 
+# Changelog
+# ----------------------
+# 
+# Version 1.0
+#  - Initial Release
+#
+# Thanks to Victron for their open platform!
+
+# TODO:
+# - AC Current auf SunSpec register wegen flußrichtung umstellen
+# - L3 U und P passen nicht zusammen
+
+import logging
+import device
+import probe
+from register import *
+
+log = logging.getLogger()
+
+class Reg_u64b(Reg_num):
+    def __init__(self, base, *args, **kwargs):
+        super(Reg_u64b, self).__init__(base, 4, *args, **kwargs)
+        self.coding = ('>Q', '>4H')
+        self.scale = float(self.scale)
+
+#nr_phases = [ 1, 3, 3 ]
+
+#phase_configs = [
+#    '1P',
+#    '3P.n',
+#    '3P',
+#]
+
+class Kostal_SmartEnergyMeter(device.EnergyMeter):
+    productid = 0x00
+    productname = 'KOSTAL Smart Energy Meter'
+    min_timeout = 0.5
+
+    def __init__(self, *args):
+        super(Kostal_SmartEnergyMeter, self).__init__(*args)
+
+        self.info_regs = [
+            Reg_u16(0x2002, '/HardwareVersion'),
+            Reg_u16(0x2003, '/FirmwareVersion'),
+            Reg_u16(0x2033, '/Serial'), # not populated correctly by KSEM, therefore reading it as uInt instad of String
+        ]
+
+
+    def device_init(self):
+        self.read_info()
+
+        regs = [
+            Reg_s16(0x9C97,  '/Ac/Power',             0.1,   '%.0f W'),     # Using SunSpec register with lower resolution (10W instead of 0.1W)
+            Reg_u32b(0x001A, '/Ac/Frequency',         1000,  '%.1f Hz'),
+            Reg_u64b(0x0200, '/Ac/Energy/Forward',    10000, '%.1f kWh'),
+            Reg_u64b(0x0204, '/Ac/Energy/Reverse',    10000, '%.1f kWh'),
+            
+            Reg_u32b(0x003E, '/Ac/L1/Voltage',        1000,  '%.1f V'),
+            # Reg_u32b(0x003C, '/Ac/L1/Current',        1000,  '%.3f A'),   # KSEM internal register. Higher Resolution but pos/neg on different registers
+            Reg_s16(0x9C88,  '/Ac/L1/Current',        100,   '%.2f A'),     # Using SunSpec register with lower resolution
+            Reg_s16(0x9C98,  '/Ac/L1/Power',          0.1,   '%.0f W'),     # Using SunSpec register with lower resolution
+            Reg_u64b(0x0250, '/Ac/L1/Energy/Forward', 10000, '%.1f kWh'),
+            Reg_u64b(0x0254, '/Ac/L1/Energy/Reverse', 10000, '%.1f kWh'),
+
+            Reg_u32b(0x0066, '/Ac/L2/Voltage',        1000,  '%.1f V'),
+            # Reg_u32b(0x0064, '/Ac/L2/Current',        1000,  '%.3f A'),   # KSEM internal register. Higher Resolution but pos/neg on different registers
+            Reg_s16(0x9C89,  '/Ac/L2/Current',        100,   '%.2f A'),     # Using SunSpec register with lower resolution
+            Reg_s16(0x9C99,  '/Ac/L2/Power',          0.1,   '%.0f W'),     # Using SunSpec register with lower resolution
+            Reg_u64b(0x02A0, '/Ac/L2/Energy/Forward', 10000, '%.1f kWh'),
+            Reg_u64b(0x02A4, '/Ac/L2/Energy/Reverse', 10000, '%.1f kWh'),
+
+            Reg_u32b(0x008E, '/Ac/L3/Voltage',        1000,  '%.1f V'),
+            # Reg_u32b(0x008C, '/Ac/L3/Current',        1000,  '%.3f A'),   # KSEM internal register. Higher Resolution but pos/neg on different registers
+            Reg_s16(0x9C8A,  '/Ac/L3/Current',        100,   '%.2f A'),     # Using SunSpec register with lower resolution
+            Reg_s16(0x9C9A,  '/Ac/L3/Power',          0.1,   '%.0f W'),     # Using SunSpec register with lower resolution
+            Reg_u64b(0x02F0, '/Ac/L3/Energy/Forward', 10000, '%.1f kWh'),
+            Reg_u64b(0x02F4, '/Ac/L3/Energy/Reverse', 10000, '%.1f kWh'),
+        ]
+        
+        self.data_regs = regs
+
+    def get_ident(self):
+        return 'cg_%s' % self.info['/Serial']
+
+
+models = {
+    18498: {
+        'model':    'KOSTAL_KSEM',
+        'handler':  Kostal_SmartEnergyMeter,
+    },
+}
+
+
+probe.add_handler(probe.ModelRegister(0x2001, models,
+                                      methods=['tcp'],
+                                      units=[1]))
---
+++ readme.md
@@ -0,0 +1,33 @@
+# Use KOSTAL Smart Energy Meter with Victron Systems
+
+This project enables the use of KOSTAL Smart Energy Meters (KSEM) within an Victron Envrionment. After installing this module on an VenusOS Device (like the Cerbo GX), the GX device will be able to identify and use the data of a KSEM. For example, an existing Kostal Photovoltaic System shall be extenden with a battery, controlled by a Multiplus II, there is no need of an additional Energy Meter, as the live values from the Kostal System can be used inside the Victron System.
+
+![Kostal Smart Energy Meter detected by Cerbo GX](pictures/ksem_in_victronui.png?raw=true)
+
+## Getting Started
+
+To be able to communicate with the KSEM, the KSEM must be directly connected to the local network via it's LAN port. It is not enough, that the PV Inverter is connected via LAN. Make sure that the Modbus TCP port is open (on the Webinterface of the KSEM: Settings -> Modbus TCP Settings -> Slave -> Enable TCP Slave = ON). Also, please ensure the the KSEM has always the same IP (either static IP or statically assigned by DHCP server!).
+In addition, you need root access to the GX device. The procedure is descibed in the [Venus OS Documentation](https://www.victronenergy.com/live/ccgx:root_access).
+This readme assumes, that you are familiar with basic file operations and SSH usage. I may improve the description later.
+
+### Installation
+
+ 1. SSH into the GX device
+ 2. Navigate to the modbus client folder: `cd /opt/victronenergy/dbus-modbus-client`
+ 3. Download the module: `wget https://github.com/pmcgn/dbus-modbus-client-kostal-smartmeter/blob/main/Kostal_SmartEnergyMeter.py`
+ 4. Open the file dbus-modbus-client.py in a text editor: `vi dbus-modbus-client.py`
+ 5. Add the instruction `import Kostal_SmartEnergyMeter` behind the other imports (before typing, press `i` to switch to input mode
+ 6. Save and exit vi: Press `ESC` then type `:wq` and press `Enter`
+ 7. Restart the GX device via `reboot`
+
+## Usage
+
+Open the GX Webinterface and navigate to Settings -> Modbus TCP Devices and trigger a Scan. It should now be able to find the KSEM. If the automatic detection does not work, go to Devices and press Add, to add the KSEM IP manually.
+
+## Troubleshooting
+
+ - Check that the file Kostal_SmartEnergyMeter.py exists and that it is placed into the correct dorectory
+ - Check if the import instruction in the file dbus-modbus-client.py has been saved
+ - Check if the KSEM IP is reachable from the local network
+ - Check if the Modbus TCP Service is running on the KSEM (either by checking the configuration or using Modbus TCP test tools).
+ - Ensure that the IP of the KSEM does not change after adding it to the GX device. Otherwise the communication will fail after a while.
