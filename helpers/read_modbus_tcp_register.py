7eb6acf959acf5c1ddcb52633f2b2c6b83a3e7d1
Philipp Mahler <development@pmcgn.de>
2023-02-27 23:03:03 +0100

    Added loop

--- helpers/read_modbus_tcp_register.py
+++ helpers/read_modbus_tcp_register.py
@@ -4,9 +4,9 @@ client = ModbusClient(method='tcp', host='192.168.2.53', port='502')
 
 client.connect()
 
-read=client.read_holding_registers(address = 8193, count = 1, slave = 1) 
-
+read=client.read_holding_registers(address = 8193, count = 2, slave = 1) 
 
 data=read.registers[0]
 
-print("DEC: " , data , ", HEX: " , hex(data))
+for value in read.registers:
+    print("DEC: " , value , ", HEX: " , hex(value))
