fba3457b8d8153c1b772136bbf2750bfdf8211c8
Philipp Mahler <development@pmcgn.de>
2023-12-05 20:38:56 +0100

    Updated helper Script and added it to the troubleshooting section

--- helpers/read_modbus_tcp_register.py
+++ helpers/read_modbus_tcp_register.py
@@ -1,12 +1,36 @@
 from pymodbus.client.tcp import ModbusTcpClient as ModbusClient
+import ipaddress
+from pymodbus.exceptions import ConnectionException, ModbusIOException
 
-client = ModbusClient(method='tcp', host='192.168.2.53', port='502')
+ksemIP = input("IP (v4) Address of your KOSTAl SMart Energy Meter (KSEM): ")
 
-client.connect()
+try:
+    if ipaddress.IPv4Address(ksemIP):
+        client = ModbusClient(method='tcp', host=ksemIP, port=502)
+        
+        try:
+            client.connect()
+            
+            try:
+                read=client.read_holding_registers(address = 0x2001, count = 1, slave = 1) 
+                
+                for value in read.registers:
+                    print("KSEM Device ID (Decimal): ", value)
+                    # print("DEC: ", value, ", HEX: ", hex(value))
 
-read=client.read_holding_registers(address = 8193, count = 2, slave = 1) 
+            except ModbusIOException as modbus_io_exception:
+                print(f"Modbus error while reading the register: {modbus_io_exception}")
 
-data=read.registers[0]
+        except ConnectionException as connection_exception:
+            print(f"TCP connection can't be established. Check KSEM IP. Check if Modbus Server is enabled on the KSEM.")
 
-for value in read.registers:
-    print("DEC: " , value , ", HEX: " , hex(value))
+        finally:
+            client.close()
+
+    else:
+        print(f"The provided address {ksemIP} is not a valid IPv4 address. Aborting.")
+        exit(1)
+
+except ValueError as value_error:
+    print(f"Invalid IP address format: {value_error}")
+    exit(1)
