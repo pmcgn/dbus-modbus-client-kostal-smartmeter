c1c7fc53819cbb8bc3084cd32d4c362529533bb7
Blocher Lukas (ME-SE/EAD4) <bco7rt@bosch.com>
2024-07-31 20:18:08 +0200

    removed Reg_u64b class, this should exist in victron py libs

--- Kostal_SmartEnergyMeter.py
+++ Kostal_SmartEnergyMeter.py
@@ -8,12 +8,6 @@ from register import *
 
 log = logging.getLogger()
 
-class Reg_u64b(Reg_num):
-    def __init__(self, base, *args, **kwargs):
-        super(Reg_u64b, self).__init__(base, 4, *args, **kwargs)
-        self.coding = ('>Q', '>4H')
-        self.scale = float(self.scale)
-
 #nr_phases = [ 1, 3, 3 ]
 
 #phase_configs = [
