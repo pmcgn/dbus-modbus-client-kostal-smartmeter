8a6c1e0b0f1cc7ae47f00726de00f64ff2fd46c2
Florian Siegmund <b1u3@users.noreply.github.com>
2025-05-26 17:03:00 +0200

    KSEM firmware 2.6 compatibility fix
    - Modbus power registers updated
    - dropped custom Reg_u64b implementation

--- Kostal_SmartEnergyMeter.py
+++ Kostal_SmartEnergyMeter.py
@@ -8,20 +8,6 @@ from register import *
 
 log = logging.getLogger()
 
-class Reg_u64b(Reg_num):
-    def __init__(self, base, *args, **kwargs):
-        super(Reg_u64b, self).__init__(base, 4, *args, **kwargs)
-        self.coding = ('>Q', '>4H')
-        self.scale = float(self.scale)
-
-#nr_phases = [ 1, 3, 3 ]
-
-#phase_configs = [
-#    '1P',
-#    '3P.n',
-#    '3P',
-#]
-
 class Kostal_SmartEnergyMeter(device.EnergyMeter):
     productid = 0x00
     productname = 'KOSTAL Smart Energy Meter'
@@ -41,29 +27,29 @@ class Kostal_SmartEnergyMeter(device.EnergyMeter):
         self.read_info()
 
         regs = [
-            Reg_s16(0x9C97,  '/Ac/Power',             0.1,   '%.0f W'),     # Using SunSpec register with lower resolution (10W instead of 0.1W)
+            Reg_s16(0x9C98,  '/Ac/Power',             10,   '%.0f W'),     # Using SunSpec register with lower resolution (10W instead of 0.1W)
             Reg_u32b(0x001A, '/Ac/Frequency',         1000,  '%.1f Hz'),
             Reg_u64b(0x0200, '/Ac/Energy/Forward',    10000, '%.1f kWh'),
             Reg_u64b(0x0204, '/Ac/Energy/Reverse',    10000, '%.1f kWh'),
             
             Reg_u32b(0x003E, '/Ac/L1/Voltage',        1000,  '%.1f V'),
             # Reg_u32b(0x003C, '/Ac/L1/Current',        1000,  '%.3f A'),   # KSEM internal register. Higher Resolution but pos/neg on different registers
-            Reg_s16(0x9C88,  '/Ac/L1/Current',        100,   '%.2f A'),     # Using SunSpec register with lower resolution
-            Reg_s16(0x9C98,  '/Ac/L1/Power',          0.1,   '%.0f W'),     # Using SunSpec register with lower resolution
+            Reg_s16(0x9C89,  '/Ac/L1/Current',        100,   '%.2f A'),     # Using SunSpec register with lower resolution
+            Reg_s16(0x9C99,  '/Ac/L1/Power',          10,   '%.0f W'),     # Using SunSpec register with lower resolution
             Reg_u64b(0x0250, '/Ac/L1/Energy/Forward', 10000, '%.1f kWh'),
             Reg_u64b(0x0254, '/Ac/L1/Energy/Reverse', 10000, '%.1f kWh'),
 
             Reg_u32b(0x0066, '/Ac/L2/Voltage',        1000,  '%.1f V'),
             # Reg_u32b(0x0064, '/Ac/L2/Current',        1000,  '%.3f A'),   # KSEM internal register. Higher Resolution but pos/neg on different registers
-            Reg_s16(0x9C89,  '/Ac/L2/Current',        100,   '%.2f A'),     # Using SunSpec register with lower resolution
-            Reg_s16(0x9C99,  '/Ac/L2/Power',          0.1,   '%.0f W'),     # Using SunSpec register with lower resolution
+            Reg_s16(0x9C8A,  '/Ac/L2/Current',        100,   '%.2f A'),     # Using SunSpec register with lower resolution
+            Reg_s16(0x9C9A,  '/Ac/L2/Power',          10,   '%.0f W'),     # Using SunSpec register with lower resolution
             Reg_u64b(0x02A0, '/Ac/L2/Energy/Forward', 10000, '%.1f kWh'),
             Reg_u64b(0x02A4, '/Ac/L2/Energy/Reverse', 10000, '%.1f kWh'),
 
             Reg_u32b(0x008E, '/Ac/L3/Voltage',        1000,  '%.1f V'),
             # Reg_u32b(0x008C, '/Ac/L3/Current',        1000,  '%.3f A'),   # KSEM internal register. Higher Resolution but pos/neg on different registers
-            Reg_s16(0x9C8A,  '/Ac/L3/Current',        100,   '%.2f A'),     # Using SunSpec register with lower resolution
-            Reg_s16(0x9C9A,  '/Ac/L3/Power',          0.1,   '%.0f W'),     # Using SunSpec register with lower resolution
+            Reg_s16(0x9C8B,  '/Ac/L3/Current',        100,   '%.2f A'),     # Using SunSpec register with lower resolution
+            Reg_s16(0x9C9B,  '/Ac/L3/Power',          10,   '%.0f W'),     # Using SunSpec register with lower resolution
             Reg_u64b(0x02F0, '/Ac/L3/Energy/Forward', 10000, '%.1f kWh'),
             Reg_u64b(0x02F4, '/Ac/L3/Energy/Reverse', 10000, '%.1f kWh'),
         ]
